4c3ad9e30096153ddb6d500c4341959b79794fb4
Alex Kouprin <kouprinal@yandex.ru>
2018-06-15 19:03:18 +0300

    cover + statistics

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import rectangle from "./rectangle.svg";
 import icon_magnifier from "./icon_magnifier.svg";
 import avatar from "./avatar.svg";
 import avatar_small from "./avatar_small.svg";
+import oval from "./oval.svg";
 import "./App.css";
 
 function HeaderNavPair(props) {
@@ -26,62 +27,130 @@ const header_data = [
   [icon_messages, "Messages"]
 ];
 
-class Header extends Component {
-  render() {
+function Header() {
+  return (
+    <header className="Header">
+      <div className="Header-nav">
+        <HeaderNavPair src={header_data[0][0]} text={header_data[0][1]} />
+        <HeaderNavPair src={header_data[1][0]} text={header_data[1][1]} />
+        <HeaderNavPair src={header_data[2][0]} text={header_data[2][1]} />
+        <HeaderNavPair src={header_data[3][0]} text={header_data[3][1]} />
+      </div>
+      <div className="Header-twitter">
+        <img
+          className="Header-twitter-logo"
+          src={icon_twitter}
+          alt="icon_twitter"
+        />
+      </div>
+      <div className="Header-menu">
+        <div className="Header-menu-search">
+          <div className="Header-menu-search-text">Search Twitter</div>
+          <img
+            className="Header-menu-search-magnifier"
+            src={icon_magnifier}
+            alt="icon_magnifier"
+          />
+        </div>
+        <div className="Header-menu-avatar">
+          <img
+            className="Header-menu-avatar-image"
+            src={avatar_small}
+            alt="avatar_small"
+          />
+        </div>
+        <div className="Header-menu-tweet">Tweet</div>
+      </div>
+    </header>
+  );
+}
+
+function Cover() {
+  return (
+    <div className="Cover">
+      <img src={process.env.PUBLIC_URL + "cover.png"} />
+    </div>
+  );
+}
+
+function StatisticsNavPair(props) {
+  if (props.active == true)
     return (
-      <div className="">
-        <header className="Header">
-          <div className="Header-nav">
-            <HeaderNavPair src={header_data[0][0]} text={header_data[0][1]} />
-            <HeaderNavPair src={header_data[1][0]} text={header_data[1][1]} />
-            <HeaderNavPair src={header_data[2][0]} text={header_data[2][1]} />
-            <HeaderNavPair src={header_data[3][0]} text={header_data[3][1]} />
-          </div>
-          <div className="Header-twitter">
-            <img
-              className="Header-twitter-logo"
-              src={icon_twitter}
-              alt="icon_twitter"
-            />
-          </div>
-          <div className="Header-menu">
-            <div className="Header-menu-search">
-              <div className="Header-menu-search-text">Search Twitter</div>
-              <img
-                className="Header-menu-search-magnifier"
-                src={icon_magnifier}
-                alt="icon_magnifier"
-              />
-            </div>
-            <div className="Header-menu-avatar">
-              <img
-                className="Header-menu-avatar-image"
-                src={avatar_small}
-                alt="avatar_small"
-              />
-            </div>
-            <div className="Header-menu-tweet">Tweet</div>
-          </div>
-        </header>
+      <div className="Statistics-nav-pair">
+        <div className="Statistics-nav-pair-active-text">{props.text}</div>
+        <div className="Statistics-nav-pair-active-count">{props.count}</div>
+        <div className="Statistics-nav-pair-active-bar" />
       </div>
     );
-  }
+  else
+    return (
+      <div className="Statistics-nav-pair">
+        <div className="Statistics-nav-pair-text">{props.text}</div>
+        <div className="Statistics-nav-pair-count">{props.count}</div>
+      </div>
+    );
+}
+
+const statistics_data = [
+  ["Tweets", "8,058", true],
+  ["Following", "721", false],
+  ["Followers", "1,815", false],
+  ["Likes", "460", false],
+  ["Lists", "2", false]
+];
+
+function Statistics() {
+  return (
+    <div className="Statistics">
+      <div className="Statistics-nav">
+        <StatisticsNavPair
+          text={statistics_data[0][0]}
+          count={statistics_data[0][1]}
+          active={statistics_data[0][2]}
+        />
+        <StatisticsNavPair
+          text={statistics_data[1][0]}
+          count={statistics_data[1][1]}
+          active={statistics_data[1][2]}
+        />
+        <StatisticsNavPair
+          text={statistics_data[2][0]}
+          count={statistics_data[2][1]}
+          active={statistics_data[2][2]}
+        />
+        <StatisticsNavPair
+          text={statistics_data[3][0]}
+          count={statistics_data[3][1]}
+          active={statistics_data[3][2]}
+        />
+        <StatisticsNavPair
+          text={statistics_data[4][0]}
+          count={statistics_data[4][1]}
+          active={statistics_data[4][2]}
+        />
+      </div>
+      <div className="Statistics-menu">
+        <div className="Statistics-menu-follow">Follow</div>
+        <div className="Statistics-menu-more">
+          <img className="Statistics-menu-more-oval" src={oval} alt="oval" />
+          <img className="Statistics-menu-more-oval" src={oval} alt="oval" />
+          <img className="Statistics-menu-more-oval" src={oval} alt="oval" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 class App extends Component {
   render() {
     return (
-      <div className="">
-        <header className="App-header">
-          <img src={icon_home} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="Desktop">
+        <Header />
+        <Cover />
+        <Statistics />
       </div>
     );
   }
 }
 
-export default Header;
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Header from "./App";
+import Cover from "./App";
+import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<Header />, document.getElementById("header"));
+ReactDOM.render(<App />, document.getElementById("app"));
 registerServiceWorker();
